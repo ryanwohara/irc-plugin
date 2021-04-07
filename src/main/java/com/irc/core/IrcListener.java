b80b9c226917ccdfacc7d38fee11b9a9352f8da6
Ryan William O'Hara <ryan@ryanwohara.com>
2021-04-06 21:07:19 -0600

    Update license

--- src/main/java/com/irc/core/IrcListener.java
+++ src/main/java/com/irc/core/IrcListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Adam <Adam@sigterm.info>
+ * Copyright (c) 2020, Ryan W. O'Hara <ryan@ryanwohara.com>, Adam <Adam@sigterm.info>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
