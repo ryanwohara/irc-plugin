4547f25066285e60d0a9dd87e263dd0ad4c0bbc0
Ryan William O'Hara <ryan@ryanwohara.com>
2025-09-11 11:25:14 -0600

    fix: Better chatbox detection for `

--- src/main/java/com/irc/IrcOverlay.java
+++ src/main/java/com/irc/IrcOverlay.java
@@ -4,6 +4,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.gameval.VarClientID;
 import net.runelite.api.widgets.ComponentID;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.input.MouseManager;
@@ -15,10 +16,11 @@ import net.runelite.client.ui.ColorScheme;
 import javax.inject.Inject;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.List;
 
 @Slf4j
-public class IrcOverlay extends Overlay {
+public class IrcOverlay extends Overlay implements KeyListener {
     private final Client client;
     private final IrcPanel panel;
 
@@ -39,6 +41,8 @@ public class IrcOverlay extends Overlay {
 
         setPosition(OverlayPosition.DYNAMIC);
         setLayer(OverlayLayer.ABOVE_WIDGETS);
+
+        this.client.getCanvas().addKeyListener(this);
     }
 
     public void subscribeEvents() {
@@ -61,14 +65,14 @@ public class IrcOverlay extends Overlay {
                         .getFocusOwner();
 
                 // Only process if NOT inside the text input
-                if (!focusOwner.equals(panel.inputField) && this.config.backTickNavigation()) {
+                if (!focusOwner.equals(panel.inputField) && chatboxFocused() && this.config.backTickNavigation()) {
                     if (e.getKeyCode() == KeyEvent.VK_BACK_QUOTE && (e.getModifiersEx() & KeyEvent.SHIFT_DOWN_MASK) != 0) {
-                        panel.cycleChannelBackwards();
                         e.consume();
+                        panel.cycleChannelBackwards();
                         return true;
                     } else if (e.getKeyCode() == KeyEvent.VK_BACK_QUOTE) {
-                        panel.cycleChannel();
                         e.consume();
+                        panel.cycleChannel();
                         return true;
                     }
                 }
@@ -79,9 +83,51 @@ public class IrcOverlay extends Overlay {
         });
     }
 
+    private boolean isHidden(int component) {
+        Widget w = client.getWidget(component);
+        return w == null || w.isSelfHidden();
+    }
+
+    boolean isDialogOpen() {
+        // Most chat dialogs with numerical input are added without the chatbox or its key listener being removed,
+        // so chatboxFocused() is true. The chatbox onkey script uses the following logic to ignore key presses,
+        // so we will use it too to not remap F-keys.
+        return isHidden(InterfaceID.Chatbox.MES_LAYER_HIDE) || isHidden(InterfaceID.Chatbox.CHATDISPLAY)
+                // We want to block F-key remapping in the bank pin interface too, so it does not interfere with the
+                // Keyboard Bankpin feature of the Bank plugin
+                || !isHidden(InterfaceID.BankpinKeypad.UNIVERSE);
+    }
+
+    boolean isOptionsDialogOpen() {
+        return client.getWidget(InterfaceID.Chatmenu.OPTIONS) != null;
+    }
+
+    boolean chatboxFocused() {
+        Widget chatboxParent = client.getWidget(InterfaceID.Chatbox.UNIVERSE);
+        if (chatboxParent == null || chatboxParent.getOnKeyListener() == null) {
+            return false;
+        }
+
+        // If the search box on the world map is open and focused, ~keypress_permit blocks the keypress
+        Widget worldMapSearch = client.getWidget(InterfaceID.Worldmap.MAPLIST_DISPLAY);
+        if (worldMapSearch != null && client.getVarcIntValue(VarClientID.WORLDMAP_SEARCHING) == 1) {
+            return false;
+        }
+
+        // The report interface blocks input due to 162:54 being hidden, however player/npc dialog and
+        // options do this too, and so we can't disable remapping just due to 162:54 being hidden.
+        Widget report = client.getWidget(InterfaceID.Reportabuse.UNIVERSE);
+        if (report != null) {
+            return false;
+        }
+
+        return true;
+    }
+
     private static final int CHATBOX_GROUP = 162;
     private static final int CHATBOX_MESSAGES_CHILD = 0;
     private static final int CHATAREA = InterfaceID.Chatbox.CHATAREA;
+    private static final int CHATINPUT = InterfaceID.Chatbox.INPUT;
 
     @Override
     public Dimension render(Graphics2D graphics) {
@@ -135,5 +181,46 @@ public class IrcOverlay extends Overlay {
         }
 
         return new Dimension(width, height);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (panel == null) return;
+
+        if (e.getKeyCode() == KeyEvent.VK_PAGE_UP && this.config.pageUpDownNavigation()) {
+            panel.cycleChannelBackwards();
+            e.consume();
+            return;
+        } else if (e.getKeyCode() == KeyEvent.VK_PAGE_DOWN && this.config.pageUpDownNavigation()) {
+            panel.cycleChannel();
+            e.consume();
+            return;
+        }
+
+        Component focusOwner = KeyboardFocusManager
+                .getCurrentKeyboardFocusManager()
+                .getFocusOwner();
+
+        // Only process if NOT inside the text input
+        if (!focusOwner.equals(panel.inputField) && chatboxFocused() && this.config.backTickNavigation()) {
+            if (e.getKeyCode() == KeyEvent.VK_BACK_QUOTE && (e.getModifiersEx() & KeyEvent.SHIFT_DOWN_MASK) != 0) {
+                panel.cycleChannelBackwards();
+                e.consume();
+            } else if (e.getKeyCode() == KeyEvent.VK_BACK_QUOTE) {
+                log.debug("f");
+                panel.cycleChannel();
+                e.consume();
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Unnecessary
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Unnecessary
     }
 }
